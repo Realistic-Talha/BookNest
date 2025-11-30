3395c44ed48c79d2ad9b51a58592274714f6317d
realistic-talha <talhayameen98@gmail.com>
2025-11-30 19:50:37 +0500

    Fix: Use HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
  import { ThemeProvider } from './context/ThemeContext';
  import { BookProvider } from './context/BookContext';
  import { ReservationProvider } from './context/ReservationContext';
